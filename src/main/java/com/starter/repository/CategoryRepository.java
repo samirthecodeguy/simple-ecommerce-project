dfef6ba2050eac001fcaf0b32148a5cc176b270d
samir <sammirou2014@gmail.com>
2023-12-11 19:46:27 +0000

    added findByName method

--- src/main/java/com/starter/repository/CategoryRepository.java
+++ src/main/java/com/starter/repository/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.starter.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.starter.domain.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+	Optional<Category> findByName(String categoryName);
 
 }
